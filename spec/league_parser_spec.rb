cece2bac1e027f802e56a5448c184302e4be3c94
Lane Bretschneider <lanebret85@gmail.com>
2023-08-02 14:09:37 -0700

    test: add test for readable attributes and modify test for list_teams in league_parser_spec.rb

--- spec/league_parser_spec.rb
+++ spec/league_parser_spec.rb
@@ -8,14 +8,22 @@ RSpec.describe LeagueParser do
 
       expect(league_parser).to be_a(LeagueParser)
     end
+
+    it "has readable attributes" do
+      league_parser = LeagueParser.new("")
+
+      expect(league_parser.teams_list).to eq([])
+    end
   end
 
   describe "#list_teams" do
     it "can return a list of team objects" do
       league_parser = LeagueParser.new("")
 
-      expect(league_parser.list_teams).to be_an(Array)
-      expect(league_parser.list_teams.count).to eq(32)
+      league_parser.list_teams
+
+      expect(league_parser.teams_list).to be_an(Array)
+      expect(league_parser.teams_list.count).to eq(32)
     end
   end
 end
