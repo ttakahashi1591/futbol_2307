3092027edf8cc23668d1db5fc519af681db0ecb4
Allan Evans <oneblankpage27@gmail.com>
2023-08-02 16:02:43 -0600

    Feat: Add method for most goals in game and least goals in game

--- lib/game_parser.rb
+++ lib/game_parser.rb
@@ -7,7 +7,7 @@ class GameParser
   end
 
   def get_game_info
-    contents = CSV.open "./data/game_fixture.csv", headers: true, header_converters: :symbol
+    contents = CSV.open "./data/games.csv", headers: true, header_converters: :symbol
     contents.each do |row|
       game_id = row[:game_id]
       season = row[:season]
@@ -22,5 +22,24 @@ class GameParser
       @games << game
     end
     p @games.first
+  end
+
+  def highest_total_score
+    score = 0
+    @games.each do |game|
+      sum = game.away_goals.to_i + game.home_goals.to_i
+      if sum > score
+        score = sum
+      end
+    end
+    score
+  end
+
+  def lowest_total_score
+    result = @games.min_by do |game|
+      require 'pry';binding.pry
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+    result.away_goals.to_i + result.home_goals.to_i
   end
 end
--- spec/game_parser_spec.rb
+++ spec/game_parser_spec.rb
@@ -2,11 +2,15 @@ require './lib/game'
 require './lib/game_parser'
 
 RSpec.describe GameParser do
-  it "Init and get info" do
-    game_parser = GameParser.new
-    expect(game_parser).to be_a GameParser
-    expect(game_parser.games).to eq([])
-    game_parser.get_game_info
-    expect(game_parser.games.count).to eq(32)
+  describe "#Initialize" do  
+    it "Exists and gets attributes" do
+      game_parser = GameParser.new
+      expect(game_parser).to be_a GameParser
+      expect(game_parser.games).to eq([])
+      game_parser.get_game_info
+      # expect(game_parser.games.count).to eq(32)
+      expect(game_parser.highest_total_score).to eq(11)
+      expect(game_parser.lowest_total_score).to eq(0)
+    end
   end
 end
