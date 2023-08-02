fa59921e7f2434d280ddada7de30734fbb606009
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-08-02 15:46:16 -0600

    feat: Added before block linking to csv data

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -4,6 +4,20 @@ require './lib/stat_tracker'
 # require './lib/game_results'
 
 RSpec.describe StatTracker do
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+  
   describe "#initialize" do
     it "initialize" do
       stat_tracker = StatTracker.new
