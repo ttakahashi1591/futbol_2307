4f930166a5734ec0563f05786a4241ff3df50a32
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-08-03 16:24:33 -0600

    feat: Add winningest_coach and worst_coach methods and testing in StatTracker class & add get_win_games_from_season and get_lose_games_from_season methods and testing in team_result_parcer class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,36 @@
+require './lib/game_parser'
+require './lib/team_result_parser'
 class StatTracker
 
   def initialize
 
+  end
+
+  def self.from_csv(locations)
+    self.new
+  end
+
+  def winningest_coach(season_id)
+    #temp method call, remove later
+    game_parser = GameParser.new
+    game_parser.get_game_info
+    result_parser = TeamResultParser.new
+    result_parser.get_game_info
+    season_hash = game_parser.get_season_games(season_id)
+    season_games = season_hash[true]
+    games_played_in_season = result_parser.get_win_games_from_season(season_games)
+    # require 'pry';binding.pry
+  end
+
+  def worst_coach(season_id)
+    #temp method call, remove later
+    game_parser = GameParser.new
+    game_parser.get_game_info
+    result_parser = TeamResultParser.new
+    result_parser.get_game_info
+    season_hash = game_parser.get_season_games(season_id)
+    season_games = season_hash[true]
+    games_played_in_season = result_parser.get_lose_games_from_season(season_games)
+    # require 'pry';binding.pry
   end
 end
--- spec/team_result_parser_spec.rb
+++ spec/team_result_parser_spec.rb
@@ -1,5 +1,6 @@
 require './lib/team_result'
 require './lib/team_result_parser'
+require './lib/game_parser'
 
 RSpec.describe TeamResultParser do
   describe "#Initialize" do
@@ -25,6 +26,47 @@ RSpec.describe TeamResultParser do
       expect(team_result_parser.percentage_home_wins).to eq(0.44)
       expect(team_result_parser.percentage_visitor_wins).to eq(0.36)
       expect(team_result_parser.percentage_ties).to eq (0.20)
+    end
+  end
+
+  describe "#get_win_percentage" do
+    it "provides a hash with coach and win percentage" do
+      team_result_parser = TeamResultParser.new
+      team_result_parser.get_game_info
+
+      game_parser = GameParser.new
+      game_parser.get_game_info
+      season_hash = game_parser.get_season_games("20132014")
+      season_games = season_hash[true]
+      expect(team_result_parser.get_win_percentage(season_games)).to be_a(Hash)
+    end
+  end
+
+  describe "#get_win_games_from_season" do
+    it "provides a string with the coach with the highest percentage" do
+      team_result_parser = TeamResultParser.new
+      team_result_parser.get_game_info
+
+      game_parser = GameParser.new
+      game_parser.get_game_info
+      season_hash = game_parser.get_season_games("20132014")
+      season_games = season_hash[true]
+  
+      expect(team_result_parser.get_win_games_from_season(season_games)).to eq("Claude Julien")
+    end
+  end
+
+  describe "#get_lose_games_from_season" do
+    it "provides a string with the coach with the lowest percentage" do
+      team_result_parser = TeamResultParser.new
+      team_result_parser.get_game_info
+
+      game_parser = GameParser.new
+      game_parser.get_game_info
+      season_hash = game_parser.get_season_games("20132014")
+      season_games = season_hash[true]
+
+      expect(team_result_parser.get_lose_games_from_season(season_games)).to eq("Peter Laviolette")
     end
   end
 end
