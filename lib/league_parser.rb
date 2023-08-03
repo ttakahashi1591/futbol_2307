613e2e2d70ca34fc0e286160c1ceb259ee16a09d
Terry Lamb <terrylamb@gmail.com>
2023-08-03 10:31:48 -0400

    fix: update #count_of_teams method and tests

--- lib/league_parser.rb
+++ lib/league_parser.rb
@@ -1,21 +1,27 @@
 require "csv"
 
 class LeagueParser
-  attr_reader :teams,
-              :teams_list
+  attr_reader :teams_list
 
   def initialize(teams)
-    @teams = CSV.open "./data/teams.csv", headers: true, header_converters: :symbol
     @teams_list = []
   end
-
+  
   def list_teams
-    @teams.map do |row|
+    contents = CSV.open "./data/teams.csv", headers: true, header_converters: :symbol
+    contents.each do |row|
       team_id = row[:team_id]
       team_name = row[:teamname]
       stadium = row[:stadium]
       new_team = Team.new(team_id, team_name, stadium)
       @teams_list << new_team
     end
+    p @teams_list
+  
+  end
+
+  def count_of_teams
+    require 'pry'; binding.pry
+    @teams_list.count
   end
 end
