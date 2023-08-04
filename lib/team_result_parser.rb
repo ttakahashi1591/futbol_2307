8dc502b5ca182615c36d2d8d03b3b595f2116821
Lane Bretschneider <lanebret85@gmail.com>
2023-08-03 19:02:02 -0700

    feat: add code for methods and helper methods related to highest_scoring_home_team

--- lib/team_result_parser.rb
+++ lib/team_result_parser.rb
@@ -58,8 +58,14 @@ class TeamResultParser
     (tied_games.to_f / @game_data_by_team.count.to_f).round(2)
   end
 
-  def highest_scoring_home_team
-
+  def highest_scoring_home_team(teams)
+    best_home_team = nil
+    teams.each do |team|
+      if team.team_id.to_i == highest_scoring_home_team_id
+        best_home_team = team.team_name
+      end
+    end
+    best_home_team
   end
 
   def lowest_scoring_home_team
@@ -80,7 +86,7 @@ class TeamResultParser
     
     54.times do
       @game_data_by_team.each do |game|
-        if game.hoa == home
+        if game.hoa == "home"
           if game.team_id.to_i == counter
             home_goals_hash[game.team_id.to_i] += game.goals.to_i
           end
@@ -96,10 +102,40 @@ class TeamResultParser
   end
 
   def home_games_played_per_team #helper method
+    home_games_played = Hash.new(0)
+    counter = 1
 
+    54.times do
+      @game_data_by_team.each do |game|
+        if game.team_id.to_i == counter
+          home_games_played[game.team_id.to_i] += 1
+        end
+      end
+      counter += 1
+    end
+    home_games_played
   end
 
   def visiting_games_played_per_team #helper method
+
+  end
+
+  def highest_scoring_home_team_id
+    a = alltime_goals_per_home_team
+    b = home_games_played_per_team
+    c = a.merge(b) { |team_id, goals, games| goals.to_f / games.to_f }
+    c.key(c.values.max)
+  end
+
+  def lowest_scoring_home_team_id
+
+  end
+
+  def highest_scoring_visiting_team_id
+  
+  end
+
+  def lowest_scoring_visiting_team_id
 
   end
 end
