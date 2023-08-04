1fae59fe0772b8faba845631959e4a4733cff2e1
Lane Bretschneider <lanebret85@gmail.com>
2023-08-03 18:14:08 -0700

    test: add tests including helper method tests for highest and lowest scoring home and visiting teams

--- spec/team_result_parser_spec.rb
+++ spec/team_result_parser_spec.rb
@@ -27,4 +27,136 @@ RSpec.describe TeamResultParser do
       expect(team_result_parser.percentage_ties).to eq (0.20)
     end
   end
+
+  describe "#scoring_teams" do
+    it "can return #highest_scoring_home_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(highest_scoring_home_team).to eq("Reign FC")
+    end
+
+    it "can return #lowest_scoring_home_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(lowest_scoring_home_team).to eq("Utah Royals FC")
+    end
+
+    it "can return #highest_scoring_visitor" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(highest_scoring_visitor).to eq("FC Dallas")
+    end
+
+    it "can return #lowest_scoring_visitor" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(lowest_scoring_visitor).to eq("San Jose Earthquakes")
+    end
+  end
+
+  describe "#helper_methods" do
+    it "can return #alltime_goals_per_home_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      # expected = {}
+
+      expect(team_result_parser.alltime_goals_per_home_team).to eq(expected)
+    end
+
+    it "can return #alltime_goals_per_visiting_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      # expected = {}
+
+      expect(team_result_parser.alltime_goals_per_visiting_team).to eq(expected)
+    end
+
+    it "can return #home_games_played_per_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      # expected = {}
+
+      expect(team_result_parser.home_games_played_per_team).to eq(expected)
+    end
+
+    it "can return #visiting_games_played_per_team" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      # expected = {}
+
+      expect(team_result_parser.visiting_games_played_per_team).to eq(expected)
+    end
+
+    it "can return #highest_scoring_home_team_id" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(team_result_parser.highest_scoring_home_team_id).to eq(54)
+    end
+
+    it "can return #lowest_scoring_home_team_id" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(team_result_parser.lowest_scoring_home_team_id).to eq(7)
+    end
+
+    it "can return #highest_scoring_visiting_team_id" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(team_result_parser.highest_scoring_visiting_team_id).to eq(6)
+    end
+
+    it "can return #lowest_scoring_visiting_team_id" do
+      team_result_parser = TeamResultParser.new
+      league_parser = LeagueParser.new
+
+      team_result_parser.get_game_info
+      league_parser.list_teams
+
+      expect(team_result_parser.lowest_scoring_visiting_team_id).to eq(27)
+    end
+  end
 end
